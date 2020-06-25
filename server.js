6b82438130cd8a52f89421b5e3cecd05c9d0df01
Xinyu <xinyudong1@gmail.com>
2020-06-25 11:03:50 -0400

    setting up backend and connecting to mongodb

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');// initialize our express app
 const port = 4000;
 const app = express();
+require('dotenv/config')
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://xdong82:3bAkrvlTsXCHWqVD@cluster0-pxsle.mongodb.net/Music2?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const uri = process.env.DB_CONNECTION;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  console.log(client);
   // perform actions on the collection object
   client.close();
 });
+
+//Middlewares
+app.use('/posts', () => {
+  console.log('hello, this is middleware')
+})
+
+
+app.get('/', (req, res) => {
+  console.log(req.params);
+  res.send('We are on Home');
+})
+
+app.get('/posts', (req, res) => {
+  // console.log(req.params);
+  res.send('We are on posts');
+})
 
 
 app.listen(port, () => {
